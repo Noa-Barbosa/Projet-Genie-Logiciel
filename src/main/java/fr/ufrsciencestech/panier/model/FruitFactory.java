fc877101a90381bfe69879665dab301a54fa5133
anthop0ral <97023395+anthop0ral@users.noreply.github.com>
2023-10-22 22:30:54 +0200

    Mise en place de l'architecture de la Fabrique
    Ajout de deux fichier : l'interface de la fabrique et celle en dur

---
+++ src/main/java/fr/ufrsciencestech/panier/model/FruitFactory.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ufrsciencestech.panier.model;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class FruitFactory implements ProduitFactory{
+
+    @Override
+    public Produit creerProduit(double arg0, OrigineProduit arg1, TypeProduit arg2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
