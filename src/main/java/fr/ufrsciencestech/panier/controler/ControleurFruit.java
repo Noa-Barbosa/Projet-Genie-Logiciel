2e8721ff9f252b581b843fb66fa45021a1d5a705
Noa Barbosa <noa.barbosa@hotmail.fr>
2023-10-23 14:39:03 +0200

    Ajout fonction ajout produit et retrait produit, test de creation de produit

--- src/main/java/fr/ufrsciencestech/panier/controler/ControleurFruit.java
+++ src/main/java/fr/ufrsciencestech/panier/controler/ControleurFruit.java
@@ -5,9 +5,12 @@
 package fr.ufrsciencestech.panier.controler;
 
 import fr.ufrsciencestech.panier.model.FruitSimple;
+import fr.ufrsciencestech.panier.model.JusCerise;
 import fr.ufrsciencestech.panier.model.OrigineProduit;
 import fr.ufrsciencestech.panier.model.TypeProduit;
 import fr.ufrsciencestech.panier.model.Panier;
+import fr.ufrsciencestech.panier.model.Produit;
+import fr.ufrsciencestech.panier.model.SaladeFruits;
 import fr.ufrsciencestech.panier.model.exception.PanierPleinException;
 import fr.ufrsciencestech.panier.model.exception.PanierVideException;
 import fr.ufrsciencestech.panier.view.VuePanelFruit;
@@ -26,23 +29,36 @@ public class ControleurFruit extends ControleurAbstract {
             switch(e.getActionCommand()){
                 case("+"):
                     FruitSimple fruit = new FruitSimple(5,OrigineProduit.France,TypeProduit.Banane);
-                    p.ajoutProduit(fruit);
+                    SaladeFruits salade = new SaladeFruits(5,OrigineProduit.Danemark);
+                    JusCerise jus = new JusCerise(6,OrigineProduit.Allemagne,fruit);
+                    JusCerise jusJus = new JusCerise(5,OrigineProduit.Danemark,jus);
+                    p.ajoutProduit(jusJus);
                     break;
                 case("-"):
                     p.retraitLastProduit(); 
                     break;
                 case("ajoutQteFruitVuePanelFruit"):
                     VuePanelFruit vpf1 = (VuePanelFruit) this.vg;
-                    this.p.ajoutProduit(vpf1.mProduit);
+                    this.p.ajoutProduit(vpf1.getProduit());
                     break;
                 case("retireQteFruitVuePanelFruit"):
                     VuePanelFruit vpf2 = (VuePanelFruit) this.vg;
-                    this.p.retraitProduit(vpf2.mProduit);
+                    this.p.retraitProduit(vpf2.getProduit());
                     break;
             }
         } 
         catch (PanierPleinException | PanierVideException ex) {
             Logger.getLogger(ControleurFruit.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    @Override
+    public void ajoutProduit(Produit p) throws PanierPleinException{
+        this.p.ajoutProduit(p);
+    }
+    
+    @Override
+    public void retraitProduit(Produit p){
+        this.p.retraitProduit(p);
     }
 }
